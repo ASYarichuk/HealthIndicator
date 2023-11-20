8b2f165d50727ced4813f683127f49bc150a14ca
Anekront <107873869+ASYarichuk@users.noreply.github.com>
2023-11-20 06:21:33 +0300

    correction of 3 comment

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,31 +6,31 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
-    [SerializeField] private Image _healttBar;
-    [SerializeField] private Image _healttBarSmooth;
+    [SerializeField] private Image _instantHealthChanger;
+    [SerializeField] private Image _smoothHealthChanger;
 
-    [SerializeField] private TMP_Text _healttBarText;
+    [SerializeField] private TMP_Text _textDisplayHealth;
 
     private float _timeHealthBarChange = 2f;
 
     private void Awake()
     {
-        _player.HealthChanged += ChangeHealth;
+        _player.HealthChanged += OnHealthChanged;
     }
 
     private void OnDestroy()
     {
-        _player.HealthChanged -= ChangeHealth;
+        _player.HealthChanged -= OnHealthChanged;
     }
 
-    private void ChangeHealth(float percent, int currentHealth, int maxHealth)
+    private void OnHealthChanged(float percent, int currentHealth, int maxHealth)
     {
-        _healttBar.fillAmount = percent;
-        _healttBarText.text = $"Текущее здоровье: {currentHealth} / {maxHealth}";
+        _instantHealthChanger.fillAmount = percent;
+        _textDisplayHealth.text = $"Текущее здоровье: {currentHealth} / {maxHealth}";
 
-        if (_healttBarSmooth.fillAmount != percent)
+        if (_smoothHealthChanger.fillAmount != percent)
         {
-            _healttBarSmooth.fillAmount = Mathf.Lerp(_healttBarSmooth.fillAmount, percent, _timeHealthBarChange * Time.deltaTime);
+            _smoothHealthChanger.fillAmount = Mathf.Lerp(_smoothHealthChanger.fillAmount, percent, _timeHealthBarChange * Time.deltaTime);
         }
     }
 }
